8a2ab818af97b6248799019117219b833b0bc74b
Pertti Viitala <pertti.viitala@gmail.com>
2020-12-29 19:44:41 +0200

    Move ErrorHandler as class and register in bootstrap

--- Support/lib/TextMate/errorhandler.php
+++ Support/lib/TextMate/errorhandler.php
@@ -4,161 +4,99 @@ declare(strict_types=1);
 
 namespace TextMate;
 
-function errorHandler($errno, $errstr, $errfile, $errline) {
-	if (!(error_reporting() & $errno)) {
-		return false;
-	}
-
-	$type = 'Error';
-	foreach (get_defined_constants(true)['Core'] as $key => $value) {
-		if (strncmp($key, 'E_', 2) === 0 && $value === $errno) {
-			// E_USER_ERROR -> User Error
-			$type = ucwords(str_replace('_', ' ', strtolower(substr($key, 2))));
-			break;
-		}
-	}
-
-	$trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
-	// remove our internal call to this handler
-	array_shift($trace);
-	// remove duplicate trigger_error call from the stack
-	if (($trace[0]['function'] ?? null) == 'trigger_error') {
-		array_shift($trace);
-	}
-
-	reportError(
-		$type,
-		$errstr,
-		$errfile,
-		$errline,
-		$trace,
-	);
-}
-
-function throwableHandler(\Throwable $throwable) {
-	reportError(
-		\get_class($throwable),
-		$throwable->getMessage(),
-		$throwable->getFile(),
-		$throwable->getLine(),
-		$throwable->getTrace(),
-	);
-	// any uncaught throwable will halt execution but exit with 0
-	exit(1);
-}
-
-function shutdownHandler() {
-	$error = error_get_last();
-	if (!$error) {
-		return;
-	}
-	switch ($error['type']) {
-	case E_ERROR:
-	case E_PARSE:
-	case E_CORE_ERROR:
-	case E_CORE_WARNING:
-	case E_COMPILE_ERROR:
-	case E_COMPILE_WARNING:
-		reportError(
-			'Compile time',
-			$error['message'],
-			// combine file information from error and function name from backtrace
-			[array_merge($error, debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS)[0] ?? [])],
-		);
-	}
-}
-
-function reportError(string $type, string $message, string $file, int $line, array $trace): void {
-	$trimPath = function ($path) {
-		$paths = [];
-		if (isset($_SERVER['TM_PROJECT_DIRECTORY'])) {
-			$paths[] = $_SERVER['TM_PROJECT_DIRECTORY'];
-		}
-		if (isset($_SERVER['TM_DIRECTORY'])) {
-			$paths[] = $_SERVER['TM_DIRECTORY'];
-		}
-		foreach ($paths as $base) {
-			if (str_starts_with($path, $base)) {
-				return substr($path, \strlen($base) + 1);
+class ErrorHandler {
+	public function report(string $type, string $message, ?string $file, ?int $line, ?array $trace = []): void {
+		$trimPath = function ($path) {
+			$paths = [];
+			if (isset($_SERVER['TM_PROJECT_DIRECTORY'])) {
+				$paths[] = $_SERVER['TM_PROJECT_DIRECTORY'];
 			}
+			if (isset($_SERVER['TM_DIRECTORY'])) {
+				$paths[] = $_SERVER['TM_DIRECTORY'];
+			}
+			foreach ($paths as $base) {
+				if (str_starts_with($path, $base)) {
+					return substr($path, \strlen($base) + 1);
+				}
+			}
+
+			return $path;
+		};
+
+		// add point of occurance to stack
+		$trace[] = ['function' => '{main}', 'file' => $file, 'line' => $line];
+
+		$backtrace = [];
+		foreach ($trace as $index => $trace) {
+			$method = isset($trace['class'])
+				? $trace['class'].$trace['type'].$trace['function']
+				: $trace['function'] ?? null;
+
+			// these are boring as hell and do not aid
+			if (!$file && (!$method || $method === '{closure}')) {
+				continue;
+			}
+
+			$backtrace[] = sprintf(
+				<<<HTML
+				<tr>
+					<td><a class="near" href="txmt://open?url=file://%s&line=%d">%s</a></td>
+					<td>in <strong>%s</strong> at line %d</td>
+				</tr>
+				HTML,
+				htmlentities($file),
+				$line,
+				htmlentities($method),
+				htmlentities($file ? $trimPath($file) : 'unknown'),
+				$line,
+			);
+
+			// these are overwritten by purpose to offset file/line for humans
+			$file = $trace['file'] ?? $file;
+			$line = $trace['line'] ?? $line;
 		}
 
-		return $path;
-	};
-
-	// add point of occurance to stack
-	$trace[] = ['function' => '{main}', 'file' => $file, 'line' => $line];
-
-	$backtrace = [];
-	foreach ($trace as $index => $trace) {
-		$method = isset($trace['class'])
-			? $trace['class'].$trace['type'].$trace['function']
-			: $trace['function'] ?? null;
-
-		// these are boring as hell and do not aid
-		if (!$file && (!$method || $method === '{closure}')) {
-			continue;
-		}
-
-		$backtrace[] = sprintf(
+		$backtrace = sprintf(
 			<<<HTML
-			<tr>
-				<td><a class="near" href="txmt://open?url=file://%s&line=%d">%s</a></td>
-				<td>in <strong>%s</strong> at line %d</td>
-			</tr>
+			<blockquote>
+				<table border="0" cellspacing="4" cellpadding="0">
+					%s
+				</table>
+			</blockquote>
 			HTML,
-			htmlentities($file),
-			$line,
-			htmlentities($method),
-			htmlentities($file ? $trimPath($file) : 'unknown'),
-			$line,
+			implode("\n", $backtrace),
 		);
 
-		// these are overwritten by purpose to offset file/line for humans
-		$file = $trace['file'] ?? $file;
-		$line = $trace['line'] ?? $line;
-	}
-
-	$backtrace = sprintf(
-		<<<HTML
-		<blockquote>
-			<table border="0" cellspacing="4" cellpadding="0">
-				%s
-			</table>
-		</blockquote>
-		HTML,
-		implode("\n", $backtrace),
-	);
-
-	$handle = null;
-	fwrite(
-		isset($_SERVER['TM_ERROR_FD'])
-			? $handle = fopen(sprintf('php://fd/%d', $_SERVER['TM_ERROR_FD']), 'w')
-			: STDOUT,
-		sprintf(
-			<<<HTML
-			<div id="exception_report" class="framed">
-				<p id="exception">
-					<strong>%s</strong>
+		$handle = null;
+		fwrite(
+			isset($_SERVER['TM_ERROR_FD'])
+				? $handle = fopen(sprintf('php://fd/%d', $_SERVER['TM_ERROR_FD']), 'w')
+				: STDOUT,
+			sprintf(
+				<<<HTML
+				<div id="exception_report" class="framed">
+					<p id="exception">
+						<strong>%s</strong>
+						%s
+					</p>
 					%s
-				</p>
-				%s
-			</div>
-			HTML,
-			htmlentities($type),
-			htmlentities($message),
-			$backtrace,
-		)
-	);
+				</div>
+				HTML,
+				htmlentities($type),
+				htmlentities($message),
+				$backtrace,
+			)
+		);
 
-	if ($handle) {
-		fclose($handle);
-	}
+		if ($handle) {
+			fclose($handle);
+		}
 
-	// TextMate goes unresponsive if you try to write too many times to error file descriptor
-	static $counter = 0;
-	if (++$counter >= ($_SERVER['TM_PHP_ERROR_LIMIT'] ?? 5)) {
-		trigger_error('Limiting errors to 5, stopping execution', E_USER_ERROR);
-		exit(1);
+		// TextMate goes unresponsive if you try to write too many times to error file descriptor
+		static $counter = 0;
+		if (++$counter >= ($_SERVER['TM_PHP_ERROR_LIMIT'] ?? 5)) {
+			trigger_error('Limiting errors to 5, stopping execution', E_USER_ERROR);
+			exit(1);
+		}
 	}
 }
--- Support/lib/bootstrap.php
+++ Support/lib/bootstrap.php
@@ -3,14 +3,72 @@
 declare(strict_types=1);
 
 require_once __DIR__.'/php80-polyfill.php';
-require_once __DIR__.'/TextMate/errorhandler.php';
 
-set_error_handler('TextMate\errorHandler');
-set_exception_handler('TextMate\throwableHandler');
-register_shutdown_function('TextMate\shutdownHandler');
-
+// resolves class TextMate/Bar into $TM_BUNDLE_SUPPORT/Support/lib/TextMate/Bar.php
 spl_autoload_register(function (string $class): void {
-	if (str_starts_with($class, 'TextMate')) {
-		require sprintf('%s/%s.php', __DIR__, str_replace('\\', '/', $class));
+	// being a polite and only handling our own namespace
+	if (str_starts_with($class, 'TextMate\\')) {
+		// autoloaders generally should use absolute paths and avoid *_once -functions
+		require __DIR__.'/'.str_replace('\\', '/', $class).'.php';
 	}
+});
+
+set_error_handler(function (int $errno, string $errstr, ?string $errfile, ?int $errline): bool {
+	// respect configuration error levels
+	if (!(error_reporting() & $errno)) {
+		return false;
+	}
+
+	$type = 'Error';
+	foreach (get_defined_constants(true)['Core'] as $key => $value) {
+		if (strncmp($key, 'E_', 2) === 0 && $value === $errno) {
+			// E_USER_ERROR -> User Error
+			$type = ucwords(str_replace('_', ' ', strtolower(substr($key, 2))));
+			break;
+		}
+	}
+
+	$trace = debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS);
+	// remove our internal call to this handler
+	array_shift($trace);
+	// remove duplicate trigger_error call from the stack
+	if (($trace[0]['function'] ?? null) == 'trigger_error') {
+		array_shift($trace);
+	}
+
+	(new TextMate\ErrorHandler())->report(
+		$type,
+		$errstr,
+		$errfile,
+		$errline,
+		$trace,
+	);
+
+	return true;
+});
+
+set_exception_handler(function (Throwable $throwable): void {
+	(new TextMate\ErrorHandler())->report(
+		get_class($throwable),
+		$throwable->getMessage(),
+		$throwable->getFile(),
+		$throwable->getLine(),
+		$throwable->getTrace(),
+	);
+	// any uncaught throwable will halt execution but exit with 0
+	exit(1);
+});
+
+register_shutdown_function(function (): void {
+	$error = error_get_last();
+	if (!$error) {
+		return;
+	}
+	(new TextMate\ErrorHandler())->report(
+		'Shutdown error',
+		$error['message'],
+		$error['file'],
+		$error['line'],
+		debug_backtrace(DEBUG_BACKTRACE_IGNORE_ARGS),
+	);
 });
