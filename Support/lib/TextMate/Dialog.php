1d0bba22ee00a3bb3acd2af849b0ccdea225ec9a
Pertti Viitala <pertti.viitala@gmail.com>
2022-01-12 18:51:14 +0200

    Update tooltip html styles

--- Support/lib/TextMate/Dialog.php
+++ Support/lib/TextMate/Dialog.php
@@ -17,33 +17,21 @@ class Dialog {
 			<style>
 				body {
 					position: absolute;
-					top: -20px;
-					left: -20px;
 					font-size: 13px;
 					font-weight: 500;
 					font-family: "Fira Code", "Menlo";
-					background-color: transparent;
 				}
 				div {
 					animation: fadeIn linear 0.15s;
 					padding: 8px;
-					margin: 40px 40px;
 					color: #eee;
 					background-color: #202020;
-					box-shadow: 0 11px 35px 2px rgba(0, 0, 0, 0.56);
 					border: 1px outset #000;
 					border-radius: 8px;
 				}
 				p {
 					white-space: pre-wrap;
 					margin: 0.5em 0 0 0;
-				}
-				.lines p:first-child {
-					font-weight: bold;
-				}
-				.lines p:nth-child(2) {
-					padding-top: 0.5em;
-					border-top: 1px solid #444;
 				}
 				.lines p:last-child {
 					padding-bottom: 0.5em;
